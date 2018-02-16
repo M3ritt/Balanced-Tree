4940f41fb21c778e8364e323ed9ca0746874b97d
Tommy <thanley@oswego.edu>
2018-02-16 16:20:12 -0500

    xml hw2 work

--- Main.java
+++ Main.java
@@ -1,8 +1,19 @@
 import java.util.ArrayList;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import java.util.Scanner;
 public class Main{
 
     public static void main(String[] args){
-//<<<<<<< HEAD
+/*//<<<<<<< HEAD
         // User test = new User("test");
 
         // test.setPassword();
@@ -30,5 +41,23 @@ public class Main{
         System.out.println("Second test username: " + secondTest.getUserName());
         System.out.println("Passwords in order: " + test.getPassword() + ", " + secondTest.getPassword());
 //>>>>>>> 2a57c8a34aeafc30ebf49b9e799a8a038ff904d9
+*/
+
+        Scanner sc = new Scanner(System.in);
+        SAXParserFactory spf = SAXParserFactory.newInstance();
+        
+        System.out.println("Enter the name of the file: ");
+        String fileName = sc.nextLine();
+        
+        try{
+            InputStream xmlInput = new FileInputStream(fileName);
+            SAXParser saxParser = spf.newSAXParser();
+            InventoryParser ixmlp = new InventoryParser();
+            saxParser.parse(xmlInput, ixmlp);
+
+        }
+        catch(SAXException|ParserConfigurationException|IOException e){
+            e.printStackTrace();
+        }
     }
 }
