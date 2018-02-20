59ff19ec0acdc5e2fed51c610284ded8e47c886a
Jondn Tryniski <jtrynisk@oswego.edu>
2018-02-20 12:20:48 -0500

    some initial thoughts on register

--- src/main/java/Register.java
+++ src/main/java/Register.java
@@ -1,3 +1,13 @@
+import java.util.ArrayList;
+
 public class Register {
 
+	private Item temp = new Item();
+	private ArrayList<Item> iList;
+	private Inventory inventory = new Inventory(iList);
+	
+	//For the register do we want it to load all inventories or just the inventory of a certain department? If so we would could have a couple different
+	//constructors in order to change it. I.E. (One constructor takes no inventory, one takes one, the other takes arrarylist of inventories, maybe
+	// a hashmap. 
+	
 }
