8a1ccdb5c8f284b627f07f4342f12cd1072c0b4c
Jondn Tryniski <jtrynisk@oswego.edu>
2018-02-20 12:48:05 -0500

    created constructor

--- src/main/java/Register.java
+++ src/main/java/Register.java
@@ -3,11 +3,15 @@ import java.util.ArrayList;
 public class Register {
 
 	private Item temp = new Item();
-	private ArrayList<Item> iList;
-	private Inventory inventory = new Inventory(iList);
+	//private ArrayList<Item> iList;
+	private Inventory inventory;
 	
 	//For the register do we want it to load all inventories or just the inventory of a certain department? If so we would could have a couple different
 	//constructors in order to change it. I.E. (One constructor takes no inventory, one takes one, the other takes arrarylist of inventories, maybe
 	// a hashmap. 
+	
+	public Register(Inventory inventory) {
+		this.inventory = inventory;
+	}
 	
 }
